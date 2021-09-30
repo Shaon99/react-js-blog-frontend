2666e5b1227a0e839237f1761cf95d9a0dfabc99
Shaon Ahmed <51782102+Shaon99@users.noreply.github.com>
2021-09-30 14:11:36 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-axios.defaults.baseURL="http://localhost:8000/";
+axios.defaults.baseURL="https://shaon-blog.herokuapp.com/";
 
 ReactDOM.render(
   <React.StrictMode>
